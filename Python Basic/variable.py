ad2aa476037fff039ad26be230a597dd998b5825
Wasif Hossain <wasif.hx@gmail.com>
2024-12-21 01:52:34 +0600

    Variable add

---
+++ Python Basic/variable.py
@@ -0,0 +1,30 @@
+a = 8 
+b = 5.63
+c = True
+d = "Bangladesh"
+e = 3 + 7j
+
+print(a,b,c,d,e)
+print(type(a),type(b),type(c),type(d),type(e))
+print(id(a),id(b),id(c),id(d),id(e))
+
+# swap
+
+x = 6.3
+y = 2
+x,y = y,x
+print(x,y)
+
+# multiple assign
+f = g = h = 50
+print(f,g,h)
+
+
+# typeCasting
+
+j = int(b)
+print(j)
+
+# deleting varibale
+
+del j
